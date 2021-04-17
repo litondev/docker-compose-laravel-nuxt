9be774206f37ca2a8a759ffbfe1aea1075d75bfd
toniwidodo123 <litoninot12345@gmail.com>
2021-04-17 10:49:59 -0700

    new

---
+++ after/frontend.sh
@@ -0,0 +1,17 @@
+echo "Frontend Testing"
+
+# npm -v
+# cd /var/www/frontend
+# pwd
+
+# cd /var/www/frontend
+# npm install
+# npm install pm2 -g
+# npm run build
+# pm2 start
+
+# exec "cd /var/www/frontend"
+# exec "npm install"
+# exec "npm install pm2 -g"
+# exec "npm run build"
+# exec "pm2 start"
---
+++ after/laravel-echo-server-cmd.sh
@@ -0,0 +1,13 @@
+echo "Laravel echo server cmd Testing"
+
+# npm -v
+# cd /var/www/laravel-echo-server
+# pwd
+
+# cd /var/www/laravel-echo-server
+# npm install laravel-echo-server -g
+# laravel-echo-server start
+
+# exec "cd /var/www/laravel-echo-server"
+# exec "npm install laravel-echo-server -g"
+# exec "laravel-echo-server start"
